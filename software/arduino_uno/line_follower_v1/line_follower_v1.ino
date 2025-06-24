7101a4b867cc62674b8e3a4e3e7e9416770f0a5c
ryandri1314 <ngvduong1113@gmail.com>
2025-06-25 00:06:07 +0700

    tunned PID

--- software/arduino_uno/line_follower_v1/line_follower_v1.ino
+++ software/arduino_uno/line_follower_v1/line_follower_v1.ino
@@ -8,21 +8,32 @@
 #define pwm_motor_bl 9
 #define pwm_motor_br 6 
 
-const float Kp = 0.25;
-const float Ki = 0.05;
-const float Kd = 0.0001;
+/*  PID variables   */
+const float Kp = 2.8;
+const float Ki = 0.15;
+const float Kd = 0.02u77i9oo;
 const float desired_pos = 0.0;
-const float dt = 0.01;
-uint8_t line_left_pos, line_right_pos, line_center_pos;
-float current_pos, prev_pos, error, derivative, output, prev_error;
-float integral = 0;
-unsigned long encoder_left_count = 0, encoder_right_count = 0;
-unsigned long last_time = 0;
-uint8_t sp_fl = 128, sp_fr = 128, sp_bl = 128, sp_br = 128;
-unsigned long last_time_reset;
-uint8_t time_reset = 100000;
+float error, derivative, output, prev_error, dt;
+float integral = 0.0;
+uint32_t time_reset = 10000;
 
-bool my_delay(uint8_t time) {
+/*  Line detecter variables  */
+int line_left_pos, line_right_pos, line_center_pos;
+float current_pos, prev_pos;
+
+/*  Encoder variables  */
+unsigned long encoder_left_count = 0, encoder_right_count = 0;
+
+/*  MyDelay variables  */
+unsigned long last_time = 0;
+unsigned long last_time_reset, last_PID_loop_time;
+
+/*  Motor variables  */
+const float scale = 22.2;
+uint16_t base_sp = 90;
+int delta;
+
+bool my_delay(uint32_t time) {
   if (millis() - last_time > time) {
     last_time = millis();
     return true;
@@ -32,6 +43,10 @@ bool my_delay(uint8_t time) {
 
 void PID_control() {
   error = desired_pos - current_pos;
+  unsigned long now = millis();
+  dt = (now - last_PID_loop_time) / 1000.0;
+  if (dt <= 0) dt = 0.01;
+  last_PID_loop_time = now;
   if (millis() - last_time_reset > time_reset) {
     last_time_reset = millis();
     integral = 0;
@@ -40,56 +55,29 @@ void PID_control() {
   }
   derivative = (error - prev_error) / dt;
   output = Kp*error + Ki*integral + Kd*derivative;
+  // if (abs(error) >= 1.0) {
+  //   output *= 1.5;
+  // }
   prev_error = error;
 }
 
 void motor_control() {
-  if (output > 0) {
-    sp_fr += output;
-    if (sp_fr > 255) {
-      sp_fr = 255;
-    }
-    sp_br += output;
-    if (sp_br > 255) {
-      sp_br = 255;
-    }
-    sp_fl -= output;
-    if (sp_fl < 0) {
-      sp_fl = 0;
-    }
-    sp_bl -= output;
-    if (sp_bl < 0) {
-      sp_bl = 0;
-    }
-  } else {
-    sp_fl += output;
-    if (sp_fl > 255) {
-      sp_fl = 255;
-    }
-    sp_bl += output;
-    if (sp_bl > 255) {
-      sp_bl = 255;
-    }
-    sp_fr -= output;
-    if (sp_fr < 0) {
-      sp_fr = 0;
-    }
-    sp_br -= output;
-    if (sp_br < 0) {
-      sp_br = 0;
-    }
-  }
+  delta = constrain(output * scale, -100, 100);
+  analogWrite(pwm_motor_fl, constrain(base_sp - delta, 0, 255));
+  analogWrite(pwm_motor_fr, constrain(base_sp + delta, 0, 255));
+  analogWrite(pwm_motor_bl, constrain(base_sp - delta, 0, 255));
+  analogWrite(pwm_motor_br, constrain(base_sp + delta, 0, 255));
 }
 
 void get_current_position() {
-  line_left_pos = analogRead(line_left);
+  line_left_pos = analogRead(line_left) + 70;
   line_center_pos = analogRead(line_center);
   line_right_pos = analogRead(line_right);
 
-  uint8_t sum = line_left_pos + line_center_pos + line_right_pos;
+  uint16_t sum = line_left_pos + line_center_pos + line_right_pos;
 
   if (sum > 0) {
-    current_pos = ((line_left_pos * (-1)) + (line_center_pos * 0) + (line_right_pos * 1))/(float)sum;
+    current_pos = ((line_left_pos * (-2.0)) + (line_center_pos * 0.0) + (line_right_pos * 2.0))/(float)sum;
     prev_pos = current_pos;
   } else {
     //Control exception
@@ -98,17 +86,10 @@ void get_current_position() {
 }
 
 void car_stop() {
-  digitalWrite(pwm_motor_bl, LOW);
-  digitalWrite(pwm_motor_br, LOW);
-  digitalWrite(pwm_motor_fl, LOW);
-  digitalWrite(pwm_motor_fr, LOW);
-}
-
-void motor_set() {
-  analogWrite(pwm_motor_br, sp_br);
-  analogWrite(pwm_motor_fl, sp_fl);
-  analogWrite(pwm_motor_bl, sp_bl);
-  analogWrite(pwm_motor_fr, sp_fr);
+  analogWrite(pwm_motor_bl, 0);
+  analogWrite(pwm_motor_br, 0);
+  analogWrite(pwm_motor_fl, 0);
+  analogWrite(pwm_motor_fr, 0);
 }
 
 void motor_init() {
@@ -118,8 +99,10 @@ void motor_init() {
   pinMode(pwm_motor_br, OUTPUT);
   pinMode(encoder_left, INPUT);
   pinMode(encoder_right, INPUT);
-  car_stop();
-  motor_set();
+  analogWrite(pwm_motor_fl, base_sp);
+  analogWrite(pwm_motor_fr, base_sp);
+  analogWrite(pwm_motor_bl, base_sp);
+  analogWrite(pwm_motor_br, base_sp);
 }
 
 void line_detecter_init() {
@@ -135,20 +118,16 @@ void setup() {
 }
 
 void loop() {
-  if(my_delay(dt*5000)) {
+  if(my_delay(10)) {
     get_current_position();
-    Serial.print("Current pos: ");
-    Serial.println(current_pos);
+    // Serial.print("Current pos: ");
+    // Serial.println(current_pos);
     PID_control();
     motor_control();
-    motor_set();
-    Serial.print("Output: ");
-    Serial.println(output);
-    // Serial.print("LEFT: ");
-    // Serial.println(line_left_pos);
-    // Serial.print("CENTER: ");
-    // Serial.println(line_center_pos);
-    // Serial.print("RIGHT: ");
-    // Serial.println(line_right_pos);
+    // Serial.print("Output: ");
+    // Serial.println(delta);
+    //Serial.print("L: "); Serial.print(line_left_pos);
+    // Serial.print(" C: "); Serial.print(line_center_pos);
+    // Serial.print(" R: "); Serial.println(line_right_pos);
   }
 }
--- software/arduino_uno/line_follower_v1/motor.h
+++ software/arduino_uno/line_follower_v1/motor.h
@@ -1,4 +1,11 @@
 #ifndef MOTOR_H
 #define MOTOR_H
 
+// class Motor {
+// public:
+//   uint8_t base_sp = 120;
+  
+
+// }
+
 #endif
